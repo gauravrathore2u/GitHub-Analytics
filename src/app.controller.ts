54ceeafa71c182c4b5d7fca699a47a66c6d7c0d4
Gaurav <gauravrathore2u@gmail.com>
2025-06-07 15:53:21 +0530

    Add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({ summary: 'Check server running' })
+  @ApiResponse({ status: 200, description: 'server is up!' })
+  getServerStatus(): string {
+    return this.appService.getServerStatus();
   }
 }
